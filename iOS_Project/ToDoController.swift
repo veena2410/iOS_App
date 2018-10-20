85fe42eda0d476b861a778ee461ebfb1016f7455
Veena <vebal13@student.sdu.dk>
2018-10-20 15:50:09 +0200

    To-Do List

---
+++ iOS_Project/ToDoController.swift
@@ -0,0 +1,56 @@
+//
+//  ToDoController.swift
+//  iOS_Project
+//
+//  Created by Veena Balakumar on 20/10/2018.
+//  Copyright © 2018 SDU. All rights reserved.
+//
+
+import UIKit
+
+class ToDoController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var myTableView: UITableView!
+    
+    var list = ["Workout", "Cleaning"]
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (list.count)
+
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return (cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCell.EditingStyle.delete
+        {
+            list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+    
+    @IBAction func addItemButton(_ sender: Any) {
+        if(input.text != "")
+        {
+            list.append(input.text!)
+            input.text = ""
+            input.resignFirstResponder()
+            myTableView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
