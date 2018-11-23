0215dbbfeee4c653dcb4fe08bc960447c1f502fd
Veena <vebal13@student.sdu.dk>
2018-11-23 07:34:07 +0100

    Code adjustments

---
+++ iOS_Project/CustomCalendar.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCalendar.swift
+//  iOS_Project
+//
+//  Created by Veena Balakumar on 22/11/2018.
+//  Copyright © 2018 SDU. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+
+class CustomCalendar: JTAppleCell {
+    
+    
+    
+}
